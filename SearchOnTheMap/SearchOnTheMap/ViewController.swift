3fd7c86979ae218cc207079b34d08abb79790b38
RinKusChi <meyuzak@gmail.com>
2022-02-12 21:45:03 +0300

    12 February 2022

--- SearchOnTheMap/SearchOnTheMap/ViewController.swift
+++ SearchOnTheMap/SearchOnTheMap/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let request = MKLocalSearch.Request()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchBar.delegate = self
+        mapView.delegate = self
+        let location = CLLocationCoordinate2D(latitude: 39.92074, longitude: 32.85409)
+        let span = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
+        let zone = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(zone, animated: true)
+        
+        request.region = mapView.region
+        
+        
+        
     }
-
-
 }
-
+extension ViewController:UISearchBarDelegate,MKMapViewDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.view.endEditing(true)
+        request.naturalLanguageQuery = searchBar.text!
+        
+        if mapView.annotations.count>0{
+            mapView.removeAnnotations(mapView.annotations)
+        }
+        
+        let search = MKLocalSearch(request: request)
+        search.start(completionHandler: {(response, error) in
+            if error != nil{
+                
+            }else if response!.mapItems.count == 0{
+                print("location null")
+            }else{
+                for loc in response!.mapItems{
+                    if let name = loc.name, let tel = loc.phoneNumber{
+                        print("name : \(name)")
+                        print("tel:  \(tel)")
+                        print("Latitude: \(loc.placemark.coordinate.latitude)")
+                        print("Longitude: \(loc.placemark.coordinate.longitude)")
+                        
+                        let pin = MKPointAnnotation()
+                        pin.coordinate = loc.placemark.coordinate
+                        pin.title = name
+                        pin.subtitle = tel
+                        self.mapView.addAnnotation(pin)
+                    }
+                }
+            }
+            
+            
+        })
+    }
+    
+}
